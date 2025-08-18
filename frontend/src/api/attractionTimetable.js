e85dec867c41bfa3eb86c48f596c04d1b19c7259
xandros15 <xandros92@gmail.com>
2025-08-18 23:45:45 +0200

    remove debug

--- frontend/src/api/attractionTimetable.js
+++ frontend/src/api/attractionTimetable.js
@@ -4,7 +4,6 @@ import {getDateTime, getNow, getTodayKonwencik} from "./clock";
 const API_URL = '/api';
 
 function filterPlaces({attr, places, whiteList}) {
-    console.log(attr.room)
     if (Array.isArray(whiteList) && whiteList.indexOf('' + attr.id) !== -1) {
         return true
     }
@@ -15,10 +14,6 @@ function filterPlaces({attr, places, whiteList}) {
 export async function getAll(places, whiteList) {
     return await fetch(API_URL)
             .then(r => r.ok ? r.json() : [])
-            .then(r => r.map(i => {
-                console.log(i.day, getTodayKonwencik())
-                return i
-            }))
             .then(r => r.filter(i => i.day === getTodayKonwencik()))
             .then(r => r.filter(attr => places ? filterPlaces({attr, places, whiteList}) : true))
             .then(r => r.map(i => {
